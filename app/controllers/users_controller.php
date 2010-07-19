a262ba8caf5b4c8f1820e12686f327c13560a26e
steve <steve@whilefalse.net>
2010-07-19 12:29:59 +0100

    sheep

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -2,47 +2,68 @@
 class UsersController extends AppController {
 
 	var $name = 'Users';
-	var $components = array('Openid.Openid');
+	var $components = array('Openid.Openid', 'Auth');
 
-    function login()
-    {
-      $returnTo = 'http://'.$_SERVER['SERVER_NAME'];
+    function login(){
+      $returnTo = 'http://'.$_SERVER['SERVER_NAME'].'/users/login/';
 
-      if (!empty($this->data))
-        {
-          try
-            {
-              $this->Openid->authenticate($this->data['OpenidUrl']['openid'], $returnTo, $returnTo, array(), true);
-            }
-          catch (InvalidArgumentException $e)
-            {
-              $this->setMessage('Invalid OpenID');
-            }
-          catch (Exception $e)
-            {
-              $this->setMessage($e->getMessage());
-            }
+      if (!empty($this->data)){
+        try{
+          $this->Openid->authenticate($this->data['OpenidUrl']['openid'], $returnTo, $returnTo, array(), true);
         }
-      elseif ($this->Openid->isOpenIDResponse())
-        {
-          $response = $this->Openid->getResponse($returnTo);
-
-          if ($response->status == Auth_OpenID_CANCEL)
-            {
-              $this->setMessage('Verification cancelled');
-            }
-          elseif ($response->status == Auth_OpenID_FAILURE)
-            {
-              $this->setMessage('OpenID verification failed: '.$response->message);
-            }
-          elseif ($response->status == Auth_OpenID_SUCCESS)
-            {
-              echo 'successfully authenticated as '.$response->identity_url.'!';
-            }
+        catch (InvalidArgumentException $e){
+          $this->_setMessage('Invalid OpenID');
         }
+        catch (Exception $e){
+          $this->_setMessage($e->getMessage());
+        }
+      }
+      elseif ($this->Openid->isOpenIDResponse()){
+        $response = $this->Openid->getResponse($returnTo);
+
+        if ($response->status == Auth_OpenID_CANCEL){
+          $this->_setMessage('Verification cancelled');
+        }
+        elseif ($response->status == Auth_OpenID_FAILURE){
+          $this->_setMessage('OpenID verification failed: '.$response->message);
+        }
+        elseif ($response->status == Auth_OpenID_SUCCESS){
+          echo 'successfully authenticated as '.$response->identity_url.'!';
+          $this->_handleLogin($response->identity_url);
+        }
+      }
     }
-    function setMessage($msg)
+
+    function logout(){
+      $this->redirect($this->Auth->logout());
+    }
+
+    function _handleLogin($openid){
+      $user = $this->User->findByOpenid($openid);
+      #Create the user if it doesn't exist
+      if (!$user){
+        $this->User->create();
+        $this->User->saveAll(array('User'=> array(
+                                                 'username' => $openid,
+                                                 'openid' => $openid,
+                                                 'password' => $this->Auth->password($openid)
+                                                 ),
+                                  'Todo' => array()
+                                  ));
+      }
+
+      #Login
+      $this->Auth->login(array('User' => array(
+                                               'username' => $openid,
+                                               'password' => $this->Auth->password($openid)
+                                               )
+                               )
+                         );
+      $this->redirect(array('controller' => 'todos', 'action' => 'view'));
+    }
+
+    function _setMessage($msg)
     {
-      $this->set($msg);
+      $this->set('message',$msg);
     }
 }
--- app/models/user.php
+++ app/models/user.php
@@ -1,6 +1,20 @@
 <?php
 class User extends AppModel {
 	var $name = 'User';
-	var $displayField = 'open_id';
+	var $displayField = 'openid';
+    var $hasOne = 'Todo';
+    var $uses = array('Todo');
 
+    function getTodoLists($user_id){
+      return array($this->Todo->findByUserId($user_id));
+    }
+    function ownsToDoList($user_id, $todo_id){
+      $todoLists = $this->getTodoLists($user_id);
+      forEach($todoLists as $todoList){
+        if ($todoList['Todo']['id'] == $todo_id){
+            return true;
+        }
+      }
+      return false;
+    }
 }
